95a3edcedf4d45277b3d4d8533a0bdac80ff7c82
Julien Desrosiers <juliend2@gmail.com>
2020-12-08 02:43:52 +0000

    move php executable into bin/

--- main.rb
+++ main.rb
@@ -1,18 +1,6 @@
 # execute like this:
 # ruby main.rb
 
-require 'dotenv/load' # loads the .env file it finds
-
-source_database = ENV['SOURCE_DATABASE_NAME']
-source_username = ENV['SOURCE_DATABASE_USERNAME']
-source_password = ENV['SOURCE_DATABASE_PASSWORD']
-source_hostname = ENV['SOURCE_DATABASE_HOSTNAME']
-output_filename = ENV['OUTPUT_FILENAME']
-# For temp db to search and replace stuff:
-localdb_user = ENV['LOCAL_DATABASE_USERNAME']
-localdb_pass = ENV['LOCAL_DATABASE_PASSWORD']
-localdb_host = 'localhost'
-
 TIMESTRING = Time.now.strftime("%Y%m%d_%H%M")
 LOCAL_DB_NAME = "tmp_db_#{TIMESTRING}"
 TMP_FILENAME = "tmp/#{TIMESTRING}"
@@ -37,7 +25,7 @@ class Replacer
   end
 
   def proceed(from, to)
-    execute("./srdb.cli.php --host=#{@localdb_host} --name=#{@localdb_name} --user=#{@localdb_user} --pass=#{@localdb_pass} --search='#{from}' --replace='#{to}'")
+    execute("./bin/srdb.cli.php --host=#{@localdb_host} --name=#{@localdb_name} --user=#{@localdb_user} --pass=#{@localdb_pass} --search='#{from}' --replace='#{to}'")
   end
 end
 
@@ -59,12 +47,17 @@ class DB
     @tmp_dbname = LOCAL_DB_NAME
     @tmp_dbuser = local_opts.fetch(:username)
     @tmp_dbpass = local_opts.fetch(:password)
+
+    @replacer = Replacer.new(@tmp_hostname, @tmp_dbname, @tmp_dbuser, @tmp_dbpass)
   end
 
-  def import_remote_to_tmp()
-    execute("mysqldump -u #{@source_username} --password=#{@source_password} #{@source_dbname} -h #{@source_hostname} --ssl-mode=disabled > #{tmp_filename}")
+  def import_remote_to_tmp(filename=nil)
+    if filename.nil?
+      filename = tmp_filename
+    end
+    execute("mysqldump -u #{@source_username} --password=#{@source_password} #{@source_dbname} -h #{@source_hostname} --ssl-mode=disabled > #{filename}")
     execute("mysql -u #{@tmp_dbuser} --password=#{@tmp_dbpass} -e 'CREATE DATABASE #{@tmp_dbname}'")
-    execute("mysql -u #{@tmp_dbuser} --password=#{@tmp_dbpass} #{@tmp_dbname} < #{tmp_filename}")
+    execute("mysql -u #{@tmp_dbuser} --password=#{@tmp_dbpass} #{@tmp_dbname} < #{filename}")
   end
 
   def dump(export_name=nil)
@@ -73,13 +66,27 @@ class DB
     end
     execute("mysqldump -u #{@tmp_dbuser} --password=#{@tmp_dbpass} #{@tmp_dbname} -h #{@tmp_hostname} > #{export_name}")
   end
+
+  def search_and_replace(from, to)
+    @replacer.proceed(from, to)
+  end
 end
+
+require 'dotenv/load' # loads the .env file it finds
+
+source_database = ENV['SOURCE_DATABASE_NAME']
+source_username = ENV['SOURCE_DATABASE_USERNAME']
+source_password = ENV['SOURCE_DATABASE_PASSWORD']
+source_hostname = ENV['SOURCE_DATABASE_HOSTNAME']
+# For temp db to search and replace stuff:
+localdb_user = ENV['LOCAL_DATABASE_USERNAME']
+localdb_pass = ENV['LOCAL_DATABASE_PASSWORD']
+localdb_host = 'localhost'
 
 db = DB.new(
   {dbname: source_database, hostname: source_hostname, username: source_username, password: source_password},
   {username: localdb_user, password: localdb_pass}
 )
-db.import_remote_to_tmp()
 
 res = execute("mysql -u #{localdb_user} --password=#{localdb_pass} -e 'CREATE DATABASE #{tmp_dbname}'")
 
