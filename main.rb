b4d874361491102f99a5d41a41668cd74b9eba9b
Julien Desrosiers <juliend2@gmail.com>
2020-12-11 17:52:58 +0000

    dev

--- main.rb
+++ main.rb
@@ -2,9 +2,7 @@
 #
 # bundle exec ruby main.rb
 
-require 'forwardable'
-
-TIMESTRING = Time.now.strftime("%Y%m%d_%H%M%S")
+TIMESTRING = Time.now.strftime("%Y%m%d_%H%M")
 LOCAL_DB_NAME = "tmp_db_#{TIMESTRING}"
 TMP_FILENAME = "tmp/#{TIMESTRING}"
 
@@ -13,12 +11,10 @@ def tmp_filename
   "#{TMP_FILENAME}.sql"
 end
 
-module Syscalls
-  def execute(command)
-    puts "Command will be executed:"
-    puts command
-    `#{command}`
-  end
+def execute(command)
+  puts "Command will be executed:"
+  puts command
+  `#{command}`
 end
 
 class Replacer
@@ -48,85 +44,28 @@ class DBConfig
   attr_reader :name, :hostname, :username, :password
 end
 
-class Dump
-  include Syscalls
-
-  def initialize(file_path)
-    @file_path = file_path
-  end
-
-  def to_s
-    @file_path
-  end
-end
-
 class DB
-  include Syscalls
-  extend Forwardable
-
-  def initialize(opts={})
-    @config = DBConfig.new opts
-    @tmp_filename = "#{srand}.sql"
-    @tmp_filepath = "tmp/#{@tmp_filename}"
-  end
-
-  def_delegators :@config, :name, :username, :password, :hostname
-
-  def <<(db_or_dump)
-    if db_or_dump.is_a?(Dump)
-      import_dump(db_or_dump)
-    elsif db_or_dump.is_a?(DB)
-      import_db(db_or_dump)
-    else
-      raise "Unknown data type to import."
-    end
-  end
-
-  def dump(file_name)
-    d = Dump.new(file_name)
-  end
-
-  private
-
-  # Imports the Dump object into the current DB
-  #
-  # dump - behaves like a String in a string context
-  def import_dump(dump)
-    execute("mysql -u #{self.username} --password=#{self.password} #{self.name} -h #{self.hostname} < #{dump}")
-  end
-
-  def import_db(db)
-    # In two steps, to be able to debug more easily:
-    execute("mysqldump -u #{db.username} --password=#{db.password} #{db.name} -h #{db.hostname} --ssl-mode=disabled > #{@tmp_filepath}")
-    execute("mysql -u #{self.username} --password=#{self.password} #{self.name} -h #{self.hostname} < #{@tmp_filepath}")
-  end
-end
-
-
-class TmpDB < DB
-  def initialize(opts={})
-    opts[:name] = opts.fetch(:name, LOCAL_DB_NAME)
-    opts[:hostname] = opts.fetch(:hostname, 'localhost')
-    super(opts)
-    execute("mysql -u #{self.username} --password=#{self.password} -h #{self.hostname} -e 'CREATE DATABASE #{self.name}'")
-  end
-end
-
-
-class DBController
   def initialize(sourcedb, localdb)
     @sourcedb = sourcedb
     @localdb = localdb
     @replacer = Replacer.new(@localdb)
   end
 
-  def import_remote_to_tmp(filename=nil)
-    if filename.nil?
-      filename = tmp_filename
-    end
+  def import_remote_to_tmp()
+		filename = tmp_filename
     execute("mysqldump -u #{@sourcedb.username} --password=#{@sourcedb.password} #{@sourcedb.name} -h #{@sourcedb.hostname} --ssl-mode=disabled > #{filename}")
     execute("mysql -u #{@localdb.username} --password=#{@localdb.password} -e 'CREATE DATABASE #{@localdb.name}'")
+		#self.search_replace_in_file('utf8mb4_0900_ai_ci', 'utf8mb4_general_ci')
     execute("mysql -u #{@localdb.username} --password=#{@localdb.password} #{@localdb.name} < #{filename}")
+  end
+
+  def search_replace_in_file(search, replace)
+    filename = tmp_filename
+    outdata = File.read(filename).gsub(search, replace)
+
+    File.open(filename, 'w') do |out|
+      out << outdata
+    end
   end
 
   def dump(export_name=nil)
